819af1dd18f8ee2178693a8f71d995760be96ada
gigayulianti <yulianti2@gmail.com>
2026-06-13 18:38:07 +0700

    commit 14

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -32,4 +32,9 @@ mahasiswa :: mahasiswa(int iNim, string iNama){
 
 void mahasiswa :: cetak(){
 
-    
+    cout << endl << "Nim = " << nim << endl;
+    cout << "Nama = " << nama << endl;
+}
+
+
+}
